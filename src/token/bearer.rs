ccce47f27063208fcf5c9542748836a21529edd2
Andrei Nesterov <ae.nesterov@gmail.com>
2019-04-04 20:40:39 -0700

    Add error types

--- src/token/bearer.rs
+++ src/token/bearer.rs
@@ -26,19 +26,20 @@ impl Deref for Token {
 ////////////////////////////////////////////////////////////////////////////////
 
 pub mod extract {
-    use failure::{err_msg, Error};
     use http::header::HeaderValue;
+
+    use crate::Error;
 
     pub fn parse_bearer_token(header: &HeaderValue) -> Result<&str, Error> {
         let val: Vec<&str> = header
             .to_str()
-            .map_err(|_| err_msg("invalid characters in the authorization header"))?
+            .map_err(|_| Error::new("invalid characters in the authorization header"))?
             .split(' ')
             .collect();
 
         match val[..] {
             ["Bearer", ref val] => Ok(val),
-            _ => Err(err_msg(
+            _ => Err(Error::new(
                 "unsupported or invalid type of the authentication token",
             )),
         }
